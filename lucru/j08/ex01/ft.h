696402978473f71b86cb4d89720935368d7facae
matias011235 <matias_freelancer@yahoo.com>
2016-07-14 08:50:09 +0300

    day 08

---
+++ lucru/j08/ex01/ft.h
@@ -0,0 +1,9 @@
+#ifndef FT_H
+ #define FT_H
+void	ft_puchar(char c);
+void	ft_pustr(chat *str);
+int	ft_strlen(char *str);
+int	ft_strcmp(char *str1, char *str2);
+void	ft_swap(int *a,int *b);
+
+#endif
---
+++ lucru/j08/ex04/ft_point.h
@@ -0,0 +1,11 @@
+#ifndef FT_POINT_H
+ #define FT_POINT_H
+
+typedef struct s_struct
+{
+	int x;
+	int y;
+}	t_point;
+
+
+#endif
